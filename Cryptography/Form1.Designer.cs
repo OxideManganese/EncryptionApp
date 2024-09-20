09fc5c253a148ad11dc4de803d13e816a647cc9b
2291921-24 <2291921-24@A318WS15>
2024-09-20 09:19:24 +0300

    Добавьте файлы проекта.

---
+++ Cryptography/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Cryptography
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CiphertextTextBox = new System.Windows.Forms.TextBox();
+            this.PlaintextTextBox = new System.Windows.Forms.TextBox();
+            this.EncryptButton = new System.Windows.Forms.Button();
+            this.DecryptButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.SuspendLayout();
+            // 
+            // CiphertextTextBox
+            // 
+            this.CiphertextTextBox.Location = new System.Drawing.Point(12, 154);
+            this.CiphertextTextBox.Multiline = true;
+            this.CiphertextTextBox.Name = "CiphertextTextBox";
+            this.CiphertextTextBox.Size = new System.Drawing.Size(257, 55);
+            this.CiphertextTextBox.TabIndex = 0;
+            this.CiphertextTextBox.Text = "Результат";
+            this.CiphertextTextBox.TextChanged += new System.EventHandler(this.CiphertextTextBox_TextChanged);
+            // 
+            // PlaintextTextBox
+            // 
+            this.PlaintextTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.PlaintextTextBox.Location = new System.Drawing.Point(12, 57);
+            this.PlaintextTextBox.Multiline = true;
+            this.PlaintextTextBox.Name = "PlaintextTextBox";
+            this.PlaintextTextBox.Size = new System.Drawing.Size(257, 54);
+            this.PlaintextTextBox.TabIndex = 1;
+            this.PlaintextTextBox.Text = "Введите текст";
+            this.PlaintextTextBox.TextChanged += new System.EventHandler(this.PlaintextTextBox_TextChanged);
+            // 
+            // EncryptButton
+            // 
+            this.EncryptButton.Location = new System.Drawing.Point(32, 117);
+            this.EncryptButton.Name = "EncryptButton";
+            this.EncryptButton.Size = new System.Drawing.Size(109, 23);
+            this.EncryptButton.TabIndex = 2;
+            this.EncryptButton.Text = "Шифровать";
+            this.EncryptButton.UseVisualStyleBackColor = true;
+            this.EncryptButton.Click += new System.EventHandler(this.EncryptButton_Click_1);
+            // 
+            // DecryptButton
+            // 
+            this.DecryptButton.Location = new System.Drawing.Point(147, 117);
+            this.DecryptButton.Name = "DecryptButton";
+            this.DecryptButton.Size = new System.Drawing.Size(109, 23);
+            this.DecryptButton.TabIndex = 3;
+            this.DecryptButton.Text = "Дешифровать";
+            this.DecryptButton.UseVisualStyleBackColor = true;
+            this.DecryptButton.Click += new System.EventHandler(this.DecryptButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Исходный текст/шифр";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 238);
+            this.splitter1.TabIndex = 5;
+            this.splitter1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(291, 238);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DecryptButton);
+            this.Controls.Add(this.EncryptButton);
+            this.Controls.Add(this.PlaintextTextBox);
+            this.Controls.Add(this.CiphertextTextBox);
+            this.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Шифратор";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox CiphertextTextBox;
+        private System.Windows.Forms.Button EncryptButton;
+        private System.Windows.Forms.Button DecryptButton;
+        private System.Windows.Forms.TextBox PlaintextTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Splitter splitter1;
+    }
+}
+
---
+++ Cryptography/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Cryptography
+{
+    public partial class Form1 : Form
+    {
+        //переменные для хранения ключа шифрования
+        private byte[] _key;
+        private byte[] _iv;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PlaintextTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        // Функция для генерации ключа
+        private byte[] GenerateKey(int keySize)
+        {
+            byte[] key = new byte[keySize / 8];
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(key);
+            }
+            return key;
+        }
+
+        // Функция для генерации вектора инициализации
+        private byte[] GenerateIV(int blockSize)
+        {
+            byte[] iv = new byte[blockSize / 8];
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(iv);
+            }
+            return iv;
+        }
+
+        private void EncryptButton_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                // Получаем текст для шифрования
+                string plaintext = PlaintextTextBox.Text;
+
+                // Создаем объект Aes для шифрования
+                using (Aes aesAlg = Aes.Create())
+                {
+                    // Генерируем ключ и вектор инициализации
+                    _key = GenerateKey(aesAlg.KeySize);
+                    _iv = GenerateIV(aesAlg.BlockSize);
+
+                    // Преобразуем текст в байтовый массив
+                    byte[] plaintextBytes = Encoding.UTF8.GetBytes(plaintext);
+
+                    // Создаем объект шифрования
+                    ICryptoTransform encryptor = aesAlg.CreateEncryptor(_key, _iv);
+
+                    // Выполняем шифрование
+                    byte[] ciphertextBytes = encryptor.TransformFinalBlock(plaintextBytes, 0, plaintextBytes.Length);
+
+                    // Преобразуем зашифрованный байтовый массив в строку
+                    string ciphertext = Convert.ToBase64String(ciphertextBytes);
+
+                    // Выводим зашифрованный текст
+                    CiphertextTextBox.Text = ciphertext;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+            }
+        }
+
+        private void DecryptButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Получаем зашифрованный текст
+                string ciphertext = PlaintextTextBox.Text;
+
+                // Создаем объект Aes для дешифрования
+                using (Aes aesAlg = Aes.Create())
+                {
+                    // Используем тот же ключ и вектор инициализации, что и при шифровании
+                    aesAlg.Key = _key;
+                    aesAlg.IV = _iv;
+
+                    // Преобразуем зашифрованный текст в байтовый массив
+                    byte[] ciphertextBytes = Convert.FromBase64String(ciphertext);
+
+                    // Создаем объект дешифрования
+                    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                    // Выполняем дешифрование
+                    byte[] plaintextBytes = decryptor.TransformFinalBlock(ciphertextBytes, 0, ciphertextBytes.Length);
+
+                    // Преобразуем дешифрованный байтовый массив в строку
+                    string plaintext = Encoding.UTF8.GetString(plaintextBytes);
+
+                    // Выводим исходный текст
+                    CiphertextTextBox.Text = plaintext;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+            }
+        }
+
+        private void CiphertextTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
